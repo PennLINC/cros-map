03a8e839559357fc809de85b4b865911ac6c131a
Jacob Vogel <jacobwvogel@gmail.com>
2021-03-04 18:44:51 -0500

    this version has been tested and works

--- bids/unquarantine_bnkbold.py
+++ bids/unquarantine_bnkbold.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+import shutil
 
 quar_dir = '/cbica/projects/rosmap_fmri/quarantine/'
 bids_dir = '/cbica/projects/rosmap_fmri/rosmap/rawdata/'
@@ -7,7 +8,7 @@ bids_dir = '/cbica/projects/rosmap_fmri/rosmap/rawdata/'
 dirs = sorted(glob(os.path.join(quar_dir,'*')))
 for d in dirs:
     _,sub = os.path.split(d)
-    if 'sub' not in sub: continue
+    if 'sub-' not in sub: continue
     # find matches
     match = glob(os.path.join(bids_dir,sub))
     newdir = os.path.join(bids_dir,sub)
@@ -16,7 +17,13 @@ for d in dirs:
         shutil.move(d,bids_dir)
     else:
         subdirs = sorted(glob(os.path.join(d,'*')))
+        if len(subdirs)==0: continue
         print('moving the following subdirectories into',sub)
         for sd in subdirs:
-            print(sd)
-            shutil.move(sd,new_dir)
+            hd,tl = os.path.split(sd)
+            bdir = os.path.join(newdir,tl)
+            if os.path.isdir(bdir):
+                print('----------------ERROR: %s already exists'%bdir)
+            else:
+                print(sd)
+                shutil.move(sd,newdir)
