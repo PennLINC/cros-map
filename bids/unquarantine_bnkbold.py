51ce192f7d97bc1eb142f3c80c9cd475505f441d
Jacob Vogel <illdopejake@gmail.com>
2021-03-04 18:00:42 -0500

    fixed up unquarantine

--- bids/unquarantine_bnkbold.py
+++ bids/unquarantine_bnkbold.py
@@ -4,10 +4,19 @@ from glob import glob
 quar_dir = '/cbica/projects/rosmap_fmri/quarantine/'
 bids_dir = '/cbica/projects/rosmap_fmri/rosmap/rawdata/'
 
-dirs = sorted(glob('quarantine/*'))
+dirs = sorted(glob(os.path.join(quar_dir,'*')))
 for d in dirs:
     _,sub = os.path.split(d)
     if 'sub' not in sub: continue
+    # find matches
+    match = glob(os.path.join(bids_dir,sub))
     newdir = os.path.join(bids_dir,sub)
-    shutil.move(d,new_dir)
-    
+    if len(match) == 0:
+        print('moving entire directory for %s'%sub)
+        shutil.move(d,bids_dir)
+    else:
+        subdirs = sorted(glob(os.path.join(d,'*')))
+        print('moving the following subdirectories into',sub)
+        for sd in subdirs:
+            print(sd)
+            shutil.move(sd,new_dir)
